f1359c631a51953083b62c288ab59bb3b71d3fdf
Jesse Lee Ross <jesseross001@gmail.com>
2023-11-14 05:41:38 +0000

    Created Repo , Created the Django Project & App , Add the app to settings.py , Created the views and URL , Test App is working in browser

---
+++ community/urls.py
@@ -0,0 +1,11 @@
+# community urls 
+from django.urls import path 
+from . import views
+
+urlpatterns = [
+    path('',views.home , name='home'),
+    path('events/',views.events,name ='events'),
+    path('about/',views.about,name='about'),
+    path('gallery/',views.gallery,name='gallery'),
+    path('booking/',views.booking,name='booking'),
+]
---
+++ community/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+# Create your views here.
+#Creating views for home , events , about , gallery and booking pages 
+
+def home(request):
+    return render(request, 'community/home.html')
+
+def events(request):
+    return render(request, 'community/events.html')
+
+def about(request):
+    return render(request,'community/about.html')
+
+
+def gallery(request):
+    return render(request, 'community/gallery.html')
+
+def booking(request):
+    return render(request, 'community/booking.html')
+
+

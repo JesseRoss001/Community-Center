5208406fa1380487f6dba93e216ccf81639df5eb
Jesse Lee Ross <jesseross001@gmail.com>
2023-11-15 19:15:29 +0000

    Create event models, views, and forms

--- community/views.py
+++ community/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .forms import GovernmentOfficialForm, InstructorForm, GeneralPublicForm
-from .models import UserProfile, GOVERNMENT_OFFICIAL, INSTRUCTOR, GENERAL_PUBLIC
+from .forms import GovernmentOfficialForm, InstructorForm, GeneralPublicForm ,EventForm
+from .models import UserProfile, GOVERNMENT_OFFICIAL, INSTRUCTOR, GENERAL_PUBLIC , Event
 from django.contrib.auth import login as auth_login
 from django.shortcuts import redirect
 
@@ -62,3 +62,17 @@ def general_public(request):
     else:
        form =GeneralPublicForm()
     return render(request,'community/register_public.html',{'form':form})
+
+def create_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST, request.FILES)
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.author = request.user
+            event.save()
+            return redirect('event_detail', event_id = event.id )
+    else:
+        form = EventForm()
+    
+    return render(request , 'events/create_event.html',{'form':form})
+    

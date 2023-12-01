d59bbbee9117617578140778ae85ab6f0c6adce6
Jesse Lee Ross <jesseross001@gmail.com>
2023-12-01 16:38:55 +0000

    Start redesign process of profile rating

--- community/__init__.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='get_range')
-def get_range(value, max_value):
-    return range(value, max_value + 1)
-
--- my_project/settings.py
+++ my_project/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap4',
     
+    
 ]
 
 MIDDLEWARE = [

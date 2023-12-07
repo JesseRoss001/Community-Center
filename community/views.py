f896cf70eb9bc8bb3d390b0cf551697e4b6ef087
Jesse Lee Ross <jesseross001@gmail.com>
2023-12-07 14:13:24 +0000

    Add 403 page

--- community/views.py
+++ community/views.py
@@ -631,6 +631,14 @@ def get_cumulative_graph_data(request):
     }
     return context
 
+
+def custom_500(request):
+    return render(request, '500.html', status=500)
+
+def custom_403(request, exception):
+    return render(request, '403.html', status=403)
+
+
 def custom_404(request, exception):
     return render(request, '404.html', status=404)
 
